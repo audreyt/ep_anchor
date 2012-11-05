f68c7913fe990d6956c0986daae6ad11ec1470b6
George Zahariev <george.zahariev@socialtext.com>
2012-11-05 03:40:32 -0800

    style

--- src/js/index.ls
+++ src/js/index.ls
@@ -4,21 +4,21 @@ export ace-post-write-dom-line-HTML = (name, args) ->
   $ args.node .find '.anchor' .each ->
     unless $ this .children 'a' .length
       a = $ '<a class="anchor">'
-      for cls in @class-name.split(/ /) when /^anchor-/.test cls
+      for cls in @class-name.split ' ' when /^anchor-/ is cls
         $ a .add-class cls
         c = cls.slice 7
-        [key, val] = if /^href/.test c
-          ['href' c.slice 4]
-        else if /^title/.test c
-          ['title' c.slice 5]
-        else if /^name/.test c
-          ['name' c.slice 4]
+        [key, val] = if /^href/ is c
+          [\href c.slice 4]
+        else if /^title/ is c
+          [\title c.slice 5]
+        else if /^name/ is c
+          [\name c.slice 4]
         $ a .attr key, decode-URI-component val
       
       $ this .replace-with <| $ a .html @inner-HTML
 
 export ace-attribs-to-classes = (name, {key}) ->
-  if /^class-anchor/.test key
+  if /^class-anchor/ is key
     [key.slice 6]
   else
     []
--- static/js/index.js
+++ static/js/index.js
@@ -5,16 +5,16 @@ out$.acePostWriteDomLineHTML = acePostWriteDomLineHTML = function(name, args){
     var a, i$, ref$, len$, cls, c, ref1$, key, val;
     if (!$(this).children('a').length) {
       a = $('<a class="anchor">');
-      for (i$ = 0, len$ = (ref$ = this.className.split(/ /)).length; i$ < len$; ++i$) {
+      for (i$ = 0, len$ = (ref$ = this.className.split(' ')).length; i$ < len$; ++i$) {
         cls = ref$[i$];
-        if (/^anchor-/.test(cls)) {
+        if (/^anchor-/.exec(cls)) {
           $(a).addClass(cls);
           c = cls.slice(7);
-          ref1$ = /^href/.test(c)
+          ref1$ = /^href/.exec(c)
             ? ['href', c.slice(4)]
-            : /^title/.test(c)
+            : /^title/.exec(c)
               ? ['title', c.slice(5)]
-              : /^name/.test(c) ? ['name', c.slice(4)] : void 8, key = ref1$[0], val = ref1$[1];
+              : /^name/.exec(c) ? ['name', c.slice(4)] : void 8, key = ref1$[0], val = ref1$[1];
           $(a).attr(key, decodeURIComponent(val));
         }
       }
@@ -25,7 +25,7 @@ out$.acePostWriteDomLineHTML = acePostWriteDomLineHTML = function(name, args){
 out$.aceAttribsToClasses = aceAttribsToClasses = function(name, arg$){
   var key;
   key = arg$.key;
-  if (/^class-anchor/.test(key)) {
+  if (/^class-anchor/.exec(key)) {
     return [key.slice(6)];
   } else {
     return [];
